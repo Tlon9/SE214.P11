a2a507c9670210b8325ce5c8710c0522864520db
LongChauThien <long.chauthien03@gmail.com>
2024-11-08 23:05:07 +0700

    Update FlightResult screen

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:travelowkey/models/flightSearch_model.dart';
+import 'package:travelowkey/models/flight_model.dart';
 
 class FlightSearchDataProvider {
   final String apiUrl;
@@ -29,5 +30,108 @@ class FlightSearchDataProvider {
     // } else {
     //   throw Exception("Failed to load flight search data");
     // }
+  }
+}
+
+class FlightResultDataProvider {
+  final String apiUrl;
+
+  FlightResultDataProvider({required this.apiUrl});
+
+  Future<List<Flight>> fetchFlightResults(
+      {Map<String, dynamic>? searchInfo}) async {
+    // Append search info as query parameters
+    // final uri = Uri.parse(apiUrl).replace(queryParameters: searchInfo);
+    // final response = await http.get(uri);
+
+    const mockResponse = '''
+      {
+        "flights": [
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "VietJet Air",
+            "departureTime": "22:50",
+            "arrivalTime": "01:00",
+            "price": 1703440,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 10m",
+            "seatClass": "Economy"
+          },
+          {
+            "name": "Vietnam Airlines",
+            "departureTime": "08:00",
+            "arrivalTime": "10:30",
+            "price": 2250000,
+            "departureLocation": "SGN",
+            "arrivalLocation": "HAN",
+            "duration": "2h 30m",
+            "seatClass": "Economy"
+          }
+        ]
+      }
+      ''';
+    final response = json.decode(mockResponse);
+    final temp = (response['flights'] as List)
+        .map((json) => Flight.fromJson(json))
+        .toList();
+    print(temp);
+    return temp;
+
+    // if (response.statusCode == 200) {
+    //   // Parse the JSON data
+    //   List<dynamic> data = json.decode(response.body);
+    //   return data.map((json) => Flight.fromJson(json)).toList();
+    // } else {
+    //   throw Exception("Failed to load flight data");
   }
 }
